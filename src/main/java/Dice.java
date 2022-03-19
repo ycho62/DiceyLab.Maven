b2508b9d1a1915c6378919b5c1043eb05d537200
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-18 21:00:09 -0400

    made dice and tosssum

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,15 @@
+import java.util.Random;
+
 public class Dice {
-
-
+    private int numberOfDies;
+    Random diceRoll = new Random();
+    public Dice(int numberOfDies) {
+        this.numberOfDies = numberOfDies;
+    }
+    public int tossAndSum(){
+        int sum = 0;
+        for(int i=1; i <= numberOfDies; i++){
+           sum +=(diceRoll.nextInt(6)+1);
+        }return sum;
+    }
 }
---
+++ src/main/java/DiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+
+class DiceTest {
+
+    @Test
+    void tossAndSum() {
+        //given
+        Dice dice = new Dice(10);
+
+
+        //when
+        System.out.println(dice.tossAndSum());
+
+        //then
+
+    }
+}
